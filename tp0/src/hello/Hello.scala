63fd1b5c3278257bfcc81f6e797fa5b867528471
XJST <ale23@hotmail.fr>
2020-04-02 19:53:54 +0200

    Initial commit

---
+++ tp0/src/hello/Hello.scala
@@ -0,0 +1,19 @@
+package hello
+/*
+ * Est-ce que App est aussi de sorte Object ?
+ * Non App est un Trait, soit un équivalent d'une interface
+ *
+ * Où est définie la méthode println?
+ * Dans l'Object Predef
+ *
+ * Quel est le type de son argument?
+ * Any ou aucun argument
+ *
+ * Quel est le type de son retour ?
+ * Unit, équivalent à void
+ *
+ *
+ */
+object Hello extends App {
+  println("Hello")
+}
---
+++ tp0/src/hello/HelloPlus.scala
@@ -0,0 +1,7 @@
+package hello
+
+object HelloPlus {
+  def main(args: Array[String]): Unit = {
+    println("Hello " + args(0))
+  }
+}
