eb4c61fe57580df6c7e519a1658f795b9b5d8177
Xabi <ale23@hotmail.fr>
2020-04-10 16:34:52 +0200

    tp1.2 done

--- tp1/src/money/Account.scala
+++ tp1/src/money/Account.scala
@@ -1,12 +1,25 @@
 package money
 
-import money.Currency.Currency
+import money._
+import Currency._
+import money.Account.Factor
 
-class Account (n: Int, currency: Currency) {
+class Account (val amount: Double, val currency: Currency) {
 
+  override def toString = s"$amount $currency"
 
+  def +(that: Account) = Account(amount + that.amount * exchangeRate(that.currency)(currency), currency)
+  def *(that: Factor)  = that * amount
 }
 
 object Account {
+  def apply(i: Double, Euro: money.Currency.Value) = new Account(i, Euro)
+  implicit def convert(that: Double): Account = double2Locale(that)
 
+  implicit class Factor(var factor: Double) {
+    override def toString = s"$factor"
+
+    def *(account: Account) = Account(factor * account.amount, account.currency)
+  }
+  def double2Factor(factor: Double): Factor = new Factor(factor)
 }
--- tp1/src/money/Currency.scala
+++ tp1/src/money/Currency.scala
@@ -2,12 +2,27 @@ package money
 
 object Currency extends Enumeration {
   type Currency = Value
-  val Euro: money.Currency.Value = Value("EUR")
-  val Dollar: money.Currency.Value = Value("USD")
-  val Yen: money.Currency.Value = Value("JPY")
+  val Euro:   money.Currency.Value  = Value("EUR")
+  val Dollar: money.Currency.Value  = Value("USD")
+  val Yen:    money.Currency.Value  = Value("JPY")
+
+  private val locale: money.Currency.Value = Euro
+
+  def exchangeRate(currency: Currency): Map[Currency, Double] = {
+    currency match {
+      case Euro   => Map(Euro   -> 1,          Dollar -> 1.15850,    Yen    -> 131.676)
+      case Dollar => Map(Euro   -> 0.863187,   Dollar -> 1,          Yen    -> 113.648)
+      case Yen    => Map(Euro   -> 0.00759441, Dollar -> 0.00879910, Yen    -> 1      )
+      case _      => throw new IllegalArgumentException(s"Unkown currency $currency.")
+    }
+  }
+
+  def double2Locale(i: Double): Account = new Account(i, locale)
 
   def main(args: Array[String]): Unit = {
-    val dollarExchangeRate = Map(Euro -> 0.863187, Dollar -> 1, Yen -> 113.648)
+    val dollarExchangeRate = Map(Euro   -> 0.863187,
+                                 Dollar -> 1,
+                                 Yen    -> 113.648)
     println(s"Euro :                     $Euro")
     println(s"DollarExchangeRate :       $dollarExchangeRate")
     println(s"DollarExchangeRate(Euro) : ${dollarExchangeRate(Euro)}")
