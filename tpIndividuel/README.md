299fc26524e6c0bad202f5a7cf0dec7543d21bb4
Xabi <ale23@hotmail.fr>
2020-05-06 20:27:08 +0200

    README added

---
+++ tpIndividuel/README.md
@@ -0,0 +1,22 @@
+#Rendu TP Individuel 
+
+* Auteur : Xavier Aleman 
+* Environement : Windows 10 Education
+* IDE : IntelliJ IDEA Ultimate
+* SDK : scala-sdk 2.13 | jdk 14
+
+## Remarques
+
+J'ai créé un trait rainfall pour mutualisé les appels aux tests du tp 
+(`src.rainfallS.rainfallS`).
+
+- 3 - Passage à Scala (`src.rainfallS.rainfallS`)
+
+- 4 - Utilisation de fonctions d'ordre supérieur (`src.rainfallOP.rainfallOP`)
+
+- 5 - Parallélisation (`src.rainfall2P.rainfall2P`)
+
+## Lancer les tests
+
+J'ai créé un objet rainfallTestSuite pour lancé tous les tests du tp en même temps 
+(`src.rainfallTestSuite.rainfallTestSuite`).
--- tpIndividuel/src/rainfall/rainfall.scala
+++ tpIndividuel/src/rainfall/rainfall.scala
@@ -14,6 +14,8 @@ trait rainfall {
   }
 
   def main(args: Array[String]): Unit = {
+    var start = System.currentTimeMillis()
+    println(s"${System.lineSeparator()}Running ${this.getClass} tests :")
     test(Nil)
     test(List(0))
     test(List(- 999 ))
@@ -22,6 +24,15 @@ trait rainfall {
     test(List(1, - 3 , 2))
     test(List(1, 2, - 3 ))
     test(List(1, - 999 , 2, - 3 ))
+//    var l: List[Int] = Nil
+//    for (w <- 0 to 1000990990) {
+//      //l = (new scala.util.Random).nextInt(999) :: l
+//      w::l
+//    }
+//    test(l)
+    println(s"${System.lineSeparator()}Execution time for ${this.getClass} tests :")
+    println(s"${System.currentTimeMillis() - start} millisecondes.")
+
   }
 }
 
